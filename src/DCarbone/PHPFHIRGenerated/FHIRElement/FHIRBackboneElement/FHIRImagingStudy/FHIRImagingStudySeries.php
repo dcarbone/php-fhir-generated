823344ab6150023590092d9dc1e3d986dedb7e9a
Daniel Paul Carbone <dcarbone@ena.com>
2019-08-17 13:17:24 -0500

    latest output from generator

--- src/DCarbone/PHPFHIRGenerated/FHIRElement/FHIRBackboneElement/FHIRImagingStudy/FHIRImagingStudySeries.php
+++ src/DCarbone/PHPFHIRGenerated/FHIRElement/FHIRBackboneElement/FHIRImagingStudy/FHIRImagingStudySeries.php
@@ -6,7 +6,7 @@ namespace DCarbone\PHPFHIRGenerated\FHIRElement\FHIRBackboneElement\FHIRImagingS
  * This class was generated with the PHPFHIR library (https://github.com/dcarbone/php-fhir) using
  * class definitions from HL7 FHIR (https://www.hl7.org/fhir/)
  * 
- * Class creation date: July 27th, 2019 15:55+0000
+ * Class creation date: August 17th, 2019 18:15+0000
  * 
  * PHPFHIR Copyright:
  * 
